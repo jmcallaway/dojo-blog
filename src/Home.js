9c1e65993f3ca7f44f53b2ae0810cc9bfc784a13
Jessie Callaway <jessie.callaway91@gmail.com>
2022-08-27 20:08:10 -0500

    created nav bar and some home page content

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+
 const Home = () => {
+	const [name, setName] = useState('mario');
+	const [age, setAge] = useState(25);
+
+	const handleClick = () => {
+		setName('luigi');
+		setAge(30);
+		console.log('hello, ninjas');
+	};
+
 	return (
 		<div className='home'>
 			<h2>Homepage</h2>
+			<p>
+				{name} is {age} years old!
+			</p>
+			<button onClick={handleClick}>Click Me!</button>
 		</div>
 	);
 };
